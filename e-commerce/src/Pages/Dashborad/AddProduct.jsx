e19735fc69d18a8d18694d6d331d49dbc434a167
Ahmed-Hossam-1 <ah0244013@gmail.com>
2023-10-25 21:07:45 +0300

    Add Upload Images

--- e-commerce/src/Pages/Dashborad/AddProduct.jsx
+++ e-commerce/src/Pages/Dashborad/AddProduct.jsx
@@ -10,6 +10,10 @@ function AddProduct() {
   const [images, setImages] = useState([]);
   const [cat, setCat] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [sentForm, setSentForm] = useState(false);
+  // Product ID
+  const [id, setId] = useState();
+  // sent real data
   const [form, setForm] = useState({
     category: "Select Category",
     title: "",
@@ -18,9 +22,20 @@ function AddProduct() {
     discount: "",
     About: "",
   });
+  // sent init data
+  const dummyData = {
+    category: null,
+    title: "dummy",
+    description: "dummy",
+    price: 55,
+    discount: 0,
+    About: "dummy",
+  };
   const nav = useNavigate();
-
+  // select images
   const openImage = useRef(null);
+  const progress = useRef([]);
+  const j = useRef(-1);
 
   // Get All CATEGORIES
   useEffect(() => {
@@ -29,21 +44,12 @@ function AddProduct() {
       .catch((err) => console.log(err));
   }, []);
 
-  async function handelSubmit(e) {
+  // sent real data
+  async function handelEdit(e) {
     e.preventDefault();
     setLoading(true);
-    let formData = new FormData();
-    formData.append("category", form.category);
-    formData.append("title", form.title);
-    formData.append("description", form.description);
-    formData.append("price", form.price);
-    formData.append("discount", form.discount);
-    formData.append("About", form.About);
-    for (let i = 0; i < images.length; ++i) {
-      formData.append("images[]", images[i]);
-    }
     try {
-      const res = await Axios.post(`${PRODUCT}/add`, formData);
+      const res = await Axios.post(`${PRODUCT}/edit/${id}`, form);
       setLoading(false);
       nav("/dashboard/products");
     } catch (err) {
@@ -51,31 +57,73 @@ function AddProduct() {
       console.log(err);
     }
   }
+  // sent init data
+  async function handelSubmitForm() {
+    try {
+      const res = await Axios.post(`${PRODUCT}/add`, dummyData);
+      setId(res.data.id);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   const handelChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
+    setSentForm(true);
+    if (sentForm !== true) {
+      handelSubmitForm();
+    }
   };
 
+  async function handelImageChange(e) {
+    setImages((prev) => [...prev, ...e.target.files]);
+    const imageFiles = e.target.files;
+    const formData = new FormData();
+    for (let i = 0; i < imageFiles.length; i++) {
+      j.current++;
+      formData.append("image", imageFiles[i]);
+      formData.append("product_id", id);
+      try {
+        const res = await Axios.post("/product-img/add", formData, {
+          onUploadProgress: (progressEvent) => {
+            const { loaded, total } = progressEvent;
+            const percent = Math.floor((loaded * 100) / total);
+            progress.current[j.current].style.width = `${percent}%`;
+            progress.current[j.current].setAttribute("percent", `${percent}%`);
+          },
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+
   // Mapping
-  const imagesShow = images.map((img, i) => (
-    <div
-      key={i}
-      className="d-flex align-items-center justify-content-start gap-2 border p-2 w-100"
-    >
-      <img width="80px" src={URL.createObjectURL(img)} />
-      <div>
-        <p className="mb-1">{img.name}</p>
-        <p className="mb-1">
-          {(img.size / 1024 < 900
-            ? img.size / 1024
-            : img.size / (1024 * 1024)
-          ).toFixed(2)}
-          {img.size / 1024 < 900 ? "KB" : "MB"}
-        </p>
+  const imagesShow = images.map((img, key) => (
+    <div key={key} className="border p-2 w-100 mb-2">
+      <div className="d-flex align-items-center justify-content-start gap-2">
+        <img width="80px" src={URL.createObjectURL(img)} />
+        <div>
+          <p className="mb-1">{img.name}</p>
+          <p className="mb-1">
+            {(img.size / 1024 < 900
+              ? img.size / 1024
+              : img.size / (1024 * 1024)
+            ).toFixed(2)}
+            {img.size / 1024 < 900 ? "KB" : "MB"}
+          </p>
+        </div>
+      </div>
+      <div className="custom-progress mt-3">
+        <span
+          ref={(e) => (progress.current[key] = e)}
+          className="inner-progress"
+        ></span>
       </div>
     </div>
   ));
   return (
-    <Form className="bg-white w-100 p-3" onSubmit={handelSubmit}>
+    <Form className="bg-white w-100 p-3" onSubmit={handelEdit}>
       <Form.Group className="mb-3" controlId="formBasicCategory">
         <Form.Label>Category</Form.Label>
         <Form.Select
@@ -101,6 +149,7 @@ function AddProduct() {
           onChange={handelChange}
           type="text"
           placeholder="Enter Title"
+          disabled={!sentForm}
         />
       </Form.Group>
 
@@ -113,6 +162,7 @@ function AddProduct() {
           onChange={handelChange}
           type="text"
           placeholder="Enter Description"
+          disabled={!sentForm}
         />
       </Form.Group>
 
@@ -125,6 +175,7 @@ function AddProduct() {
           onChange={handelChange}
           type="text"
           placeholder="Enter Price"
+          disabled={!sentForm}
         />
       </Form.Group>
 
@@ -137,6 +188,7 @@ function AddProduct() {
           onChange={handelChange}
           type="text"
           placeholder="Enter Discount"
+          disabled={!sentForm}
         />
       </Form.Group>
 
@@ -149,6 +201,7 @@ function AddProduct() {
           onChange={handelChange}
           type="text"
           placeholder="Enter About"
+          disabled={!sentForm}
         />
       </Form.Group>
 
@@ -158,22 +211,34 @@ function AddProduct() {
           ref={openImage}
           hidden
           multiple
-          onChange={(e) => setImages([...e.target.files])}
+          onChange={handelImageChange}
           type="file"
+          disabled={!sentForm}
         />
       </Form.Group>
 
       <div
         onClick={() => openImage.current.click()}
         style={{
-          border: "2px dashed #0086fe",
+          border: !sentForm ? "2px dashed gray" : "2px dashed #0086fe",
           marginBottom: "15px",
           cursor: "pointer",
         }}
         className="d-flex align-items-center justify-content gap-2 py-2 w-100 flex-column"
       >
-        <img width="100px" src="../../../public/image/upload.png" alt="NON" />
-        <p style={{ color: "#0086fe", fontWeight: "bold", margin: "0" }}>
+        <img
+          width="100px"
+          style={{ filter: !sentForm && "grayscale(1)" }}
+          src="/image/upload.png"
+          alt="NON"
+        />
+        <p
+          style={{
+            color: !sentForm ? "gray" : "#0086fe",
+            fontWeight: "bold",
+            margin: "0",
+          }}
+        >
           Upload Images
         </p>
       </div>
@@ -187,7 +252,7 @@ function AddProduct() {
         variant="primary"
         type="submit"
       >
-        {loading ? <Loading /> : "UpLoad"}
+        {loading ? <Loading /> : "Save"}
       </Button>
     </Form>
   );
