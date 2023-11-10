eeed85853c8ec42a293dd0493f662c293039d960
Ahmed Hossam <128134339+Ahmed-Hossam-1@users.noreply.github.com>
2023-11-10 13:11:49 +0200

    update

--- e-commerce/src/app/store.js
+++ e-commerce/src/app/store.js
@@ -1,9 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../Featrures/authFeature/authSlice";
-import userReducer from "../Featrures/UsersFeatrure/userSlice";
 
 const store = configureStore({
-  reducer: { auth: authReducer, user: userReducer },
+  reducer: { auth: authReducer },
   devTools: import.meta.env.VITE_NODE_ENV === "development",
 });
 
